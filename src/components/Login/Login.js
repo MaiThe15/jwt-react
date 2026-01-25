b749a190d929f4793c93f66807c61bdde9577602
MaiThe15 <maithek36@gmail.com>
2026-01-25 21:36:53 +0700

    mobile layout

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -2,22 +2,29 @@ import './Login.scss';
 
 const Login = (props) => {
     return (
-        <div className="login-container mt-3">
+        <div className="login-container">
             <div className="container">
-                <div className="row">
-                    <div className="content-left col-7">
+                <div className='brand col-12 d-block col-sm-0 d-sm-none'>
+                    JWT & REACT
+                </div>
+                <div className="row px-3 px-sm-0">
+                    <div className="content-left col-12 d-none col-sm-7 d-sm-block">
                         <div className='brand'>
-                            JWT
+                            JWT & REACT
                         </div>
                         <div className='detail'>
                             Just learning how to make a web with jwt and react
                         </div>
                     </div>
-                    <div className="content-right col-5 d-flex flex-column gap-3 py-3">
+                    <div className="content-right col-12 col-sm-5 d-flex flex-column gap-3 py-3">
                         <input type="text" className='form-control' placeholder='Email address or phone number' />
                         <input type="password" className='form-control' placeholder='Password' />
                         <button className='btn btn-primary'>Login</button>
-                        <span className='text-center'>Forgot your password?</span>
+                        <span className='text-center'>
+                            <a className="forgot-password" href="#">
+                                Forgot your password?
+                            </a>
+                        </span>
                         <hr/>
                         <div className='text-center'>
                             <button className='btn btn-success'>Create new account</button>
