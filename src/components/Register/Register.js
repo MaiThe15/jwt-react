f001111404110e09d32e15b51d2df5e5fa750053
MaiThe15 <maithek36@gmail.com>
2026-01-30 19:46:48 +0700

    add validate

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -1,19 +1,59 @@
 import './Register.scss';
 import { useHistory } from "react-router-dom";
 import axios from 'axios';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
+import { toast } from 'react-toastify';
 
 const Register = (props) => {
+    const [email, setEmail] = useState("");
+    const [phone, setPhone] = useState("");
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+    const [confirmPassword, setConfirmPassword] = useState("");
+
     let history = useHistory();
     const handleLogin = () => {
         history.push("/login");
     }
 
     useEffect(() => {
-        axios.get("http://localhost:8080/api/test-api").then(data => {
-            console.log(">>> check data axios:", data)
-        })
+        // axios.get("http://localhost:8080/api/test-api").then(data => {
+        //     console.log(">>> check data axios:", data)
+        // })
     }, []);
+
+    const isValidInputs = () => {
+        if(!email){
+            toast.error("Email is required");
+            return false;
+        }
+        if(!phone){
+            toast.error("Phone is required");
+            return false;
+        }
+        if(!password){
+            toast.error("Password is required");
+            return false;
+        }
+        if(password != confirmPassword){
+            toast.error("Your password is not the same");
+            return false;
+        }
+        let regx = /\S+@\S+\.\S+/;
+        if(!regx.test(email)){
+            toast.error("Invalid email address");
+            return false;
+        }
+        return true;
+    }
+
+    const handleRegister = () => {
+        let check = isValidInputs();
+        
+        let userData = { email, phone, username, password};
+        console.log(">>> check user data:", userData)
+    }
+
     return (
         <div className="register-container">
             <div className="container">
@@ -32,25 +72,35 @@ const Register = (props) => {
                     <div className="content-right col-12 col-sm-5 d-flex flex-column gap-3 py-3">
                         <div className='form-group'>
                             <label>Email:</label>
-                            <input type="text" className='form-control' placeholder='Email address' />
+                            <input type="text" className='form-control' placeholder='Email address' 
+                                value={email} onChange={(event) => setEmail(event.target.value)}
+                            />
                         </div>
                         <div className='form-group'>
                             <label>Phone numer:</label>
-                            <input type="text" className='form-control' placeholder='Phone numer' />
+                            <input type="text" className='form-control' placeholder='Phone numer' 
+                                value={phone} onChange={(event) => setPhone(event.target.value)}
+                            />
                         </div>
                         <div className='form-group'>
                             <label>Username:</label>
-                            <input type="text" className='form-control' placeholder='Username' />
+                            <input type="text" className='form-control' placeholder='Username' 
+                                value={username} onChange={(event) => setUsername(event.target.value)}
+                            />
                         </div>
                         <div className='form-group'>
                             <label>Password:</label>
-                            <input type="password" className='form-control' placeholder='Password' />
+                            <input type="password" className='form-control' placeholder='Password' 
+                                value={password} onChange={(event) => setPassword(event.target.value)}
+                            />
                         </div>
                         <div className='form-group'>
                             <label>Re-enter password:</label>
-                            <input type="password" className='form-control' placeholder='Re-enter password' />
+                            <input type="password" className='form-control' placeholder='Re-enter password' 
+                                value={confirmPassword} onChange={(event) => setConfirmPassword(event.target.value)}
+                            />
                         </div>
-                        <button className='btn btn-primary'>Register</button>
+                        <button className='btn btn-primary' onClick={() => handleRegister()}>Register</button>
                         <hr/>
                         <div className='text-center'>
                             <button className='btn btn-success' onClick= {() => handleLogin()}>
