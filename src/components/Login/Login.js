74ff0ba2a1993cb84e1b9c3ac50f310d8e656dd5
MaiThe15 <maithek36@gmail.com>
2026-02-06 21:45:33 +0700

    add routes

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import './Login.scss';
 import { useHistory } from "react-router-dom";
 import { toast } from "react-toastify";
@@ -20,6 +20,7 @@ const Login = (props) => {
     const handleCreateNewAccount = () => {
         history.push("/register");
     }
+
     const handleLogin = async () => {
         setObjValidInput(defaultObjValidInput);
 
@@ -42,11 +43,27 @@ const Login = (props) => {
             }
             sessionStorage.setItem('account', JSON.stringify(data));
             history.push('/users');
+            window.location.reload();
         }
         if(response && response.data && +response.data.EC !== 0){
             toast.error(response.data.EM)
         }
     }
+
+    const handlePressEnter = (event) => {
+        if(event.charCode === 13 && event.code === 'Enter'){
+            handleLogin();
+        }
+    }
+
+    useEffect(() => {
+        let session = sessionStorage.getItem('account');
+        if(session){
+            history.push('/');
+            window.location.reload();
+        }
+    }, [])
+
     return (
         <div className="login-container">
             <div className="container">
@@ -76,6 +93,7 @@ const Login = (props) => {
                         placeholder='Password'
                         value={password}
                         onChange={(event)=> {setPassword(event.target.value)}}
+                        onKeyPress={(event) => handlePressEnter(event)}
                         />
                         <button className='btn btn-primary' onClick={() => handleLogin()}>Login</button>
                         <span className='text-center'>
--- src/components/ManageUsers/Users.js
+++ src/components/ManageUsers/Users.js
@@ -1,15 +1,6 @@
 import { useEffect } from "react";
-import { useHistory } from "react-router-dom/cjs/react-router-dom.min";
 
 const Users = (pops) => {
-    let history = useHistory();
-    useEffect(() => {
-        let session = sessionStorage.getItem('account');
-        if(!session){
-            history.push('/login');
-        }
-    }, [])
-
     return(
         <div>
             Users component
