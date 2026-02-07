cf28b2715a4aac73f43b974f1a732aa944b822ac
MaiThe15 <maithek36@gmail.com>
2026-02-07 22:08:44 +0700

    show user list

--- src/services/userService.js
+++ src/services/userService.js
@@ -8,4 +8,8 @@ const loginUser = (valueLogin, password) => {
     return axios.post("http://localhost:8080/api/v1/login", {valueLogin, password})
 }
 
-export {registerNewUser, loginUser};
+const fetchAllUser = () => {
+    return axios.get("http://localhost:8080/api/v1/user/read")
+}
+
+export {registerNewUser, loginUser, fetchAllUser};
