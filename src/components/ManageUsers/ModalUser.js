301e84911e1077dc13ee28e35d5c17e03ca00d83
MaiThe15 <maithek36@gmail.com>
2026-03-06 22:17:23 +0700

    customize axios

--- src/components/ManageUsers/ModalUser.js
+++ src/components/ManageUsers/ModalUser.js
@@ -40,14 +40,14 @@ const ModalUser = (props) => {
 
     const getGroups = async () => {
         let res = await fetchGroup();
-        if(res && res.data && res.data.EC === 0){
-            setUserGroups(res.data.DT);
-            if(res.data.DT && res.data.DT.length > 0){
-                let groups = res.data.DT;
+        if(res && res.EC === 0){
+            setUserGroups(res.DT);
+            if(res.DT && res.DT.length > 0){
+                let groups = res.DT;
                 setUserData({...userData, group: groups[0].id})
             }
         } else{
-            toast.error(res.data.EM);
+            toast.error(res.EM);
         }
     }
 
@@ -96,17 +96,17 @@ const ModalUser = (props) => {
                 : await updateCurrentUser({...userData, groupId: userData['group']})
             );
             // console.log(">>>check response:", res);
-            if(res.data && res.data.EC === 0){
+            if(res && res.EC === 0){
                 props.onHide();
                 setUserData({
                     ...defaultUserData, 
                     // group: userGroups[0].id})
                     group: userGroups && userGroups.length > 0 ? userGroups[0].id : ''})
             }
-            if(res.data && res.data.EC !== 0){
-                toast.error(res.data.EM);
+            if(res && res.EC !== 0){
+                toast.error(res.EM);
                 let _validInputs = _.cloneDeep(validInputsDefault);
-                _validInputs[res.data.DT] = false;
+                _validInputs[res.DT] = false;
                 setValidInputs(_validInputs);
             }
         }
--- src/components/ManageUsers/Users.js
+++ src/components/ManageUsers/Users.js
@@ -25,10 +25,10 @@ const Users = (pops) => {
 
     const fetchUsers = async () => {
         let response = await fetchAllUser(currentPage, currentLimit);
-        if(response && response.data && response.data.EC === 0){
-            setTotalPages(response.data.DT.totalPages);
-            setListUsers(response.data.DT.users);
-
+        console.log(">>> check res:", response);
+        if(response && response.EC === 0){
+            setTotalPages(response.DT.totalPages);
+            setListUsers(response.DT.users);
         }
     }
 
@@ -50,12 +50,12 @@ const Users = (pops) => {
     const confirmDeleteUser = async () => {
         let response = await deleteUser(dataModal);
         // console.log(">>> check response:", response);
-        if(response && response.data.EC === 0){
-            toast.success(response.data.EM);
+        if(response && response.EC === 0){
+            toast.success(response.EM);
             await fetchUsers();
             setIsShowModalDelete(false);
         } else{
-            toast.error(response.data.EM);
+            toast.error(response.EM);
         }
     }
 
